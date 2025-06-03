4e5e8b4b136a46c51ddeca77c8ce94fe25113729
timmy-tuff-knuckles <judah.thornton@kingsway.school.nz>
2025-06-04 09:40:38 +1200

    fence cost calculator
    final copy of this assesment

--- #assesment 2.py
+++ #assesment 2.py
@@ -1,4 +1,4 @@
-#assesment 2
+#fence cost calculator
 #judah thornton
 #28 may 2025
 #v1
@@ -7,29 +7,43 @@
 
 loop = '' #create a loop
 while loop == '':
-    question_legth = int(input('what it the length of the desired area to be measured in M: '))
-    if question_legth>0: #do an if to make sure the number is over 0
-        print (f'the length of the desired area is {question_legth}')
-        question_width = int(input('what it the width of the desired area to be measured in M: '))
+    try:
+        question_legth = int(input('What it the length of the desired area to be measured in meters? '))
+
+        if question_legth>0: #do an if to make sure the number is over 0
+            print (f'You entered that the length of the desired area is {question_legth}.')
+            question_width = int(input('What it the width of the desired area to be measured in meters? '))
+            
+
+            if question_width>0:#ask for width than do a if
+                print (f'You entered that the width of the desired area is {question_width}.')
+                cost_per_meter = int(input('What is the cost per meter in nzd? $'))
+
+                if cost_per_meter>0:
+                    # print (f'The cost per meter of the desired area is ${cost_per_meter}\n')
+                    awnser_fence_length = ((question_width * 2)+(question_legth *2)) #put in then print the awnser 
+                    print (f'This is the length for fence you will need {awnser_fence_length}.\n')
+                    awnser_cost=(awnser_fence_length*cost_per_meter)#this will caulculate the cost then print your cost awnser 
+                    print (f'The total cost of the fencing will be: ${awnser_cost}.\n')
+                    loop = (input('if you would like to continue press <enter> or anything else to quit:\n')) #ask if they would like to finish or continue
+                    
+                        
+                else:#this will get a number above zero 
+                    print (f'the length of the desired area is {cost_per_meter}.')
+                    print ('please enter a valid number above zero.')  
+
+            else:#this will get a number above zero
+                print (f'the width of the desired area is {question_width}.')
+                print ('please enter a valid number above zero.')    
         
-        if question_width>0:#ask for width than do a if
-            print (f'the cost per meter of the desired area is {question_width}')
-            cost_per_meter = int(input('what is the cost per meter in nzd: '))
-            if cost_per_meter>0:
-                print (f'the cost per meter of the desired area is {cost_per_meter}\n')
-                awnser = (question_width+question_legth)
-                print (f'this is the length for fence you will need {awnser}\n')
-                awnser_2=(awnser*cost_per_meter)
-                print (f'this is how much it will cost for all the fencing {awnser_2}\n')
-                continue_question = (input('if you would like to continue press enter if not press enter no bellow:\n'))
-                if continue_question == 'no':
-                    loop = 'done :D'
-            else:
-                print (f'the legth of the desired area is {cost_per_meter}')
-                print ('please enter a valid number obove zero')    
-        else:
-            print (f'the width of the desired area is {question_width}')
-            print ('please enter a valid number obove zero')    
-    else:
-        print (f'the length of the desired area is {question_legth}')
-        print ('please enter a valid number obove zero')
+        else:#this will get a number above zero
+            print (f'the length of the desired area is {question_legth}.')
+            print ('please enter a valid number above zero.')
+    except ValueError:
+        print('please enter a valid number above zero.')
+
+    
+
+
+
+
